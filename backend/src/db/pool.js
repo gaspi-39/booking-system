52f4b126af8904ff977b2c7707ef9d7504358be1
Gaspar Lazarte <lazartegaspar@gmail.com>
2025-11-20 13:38:33 -0300

    Archivo de configuracion para base de datos y pool de conexiones

---
+++ backend/src/db/pool.js
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise"
+import path from "path"
+import { fileURLToPath } from "url"
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config({ path: path.join(__dirname, ".env.db") })
+
+export const pool = mysql.createPool({
+	host: DB_HOST,
+	port: DB_PORT,
+	user: DB_USER,
+	password: DB_PASSWORD,
+	database: DB_NAME,
+})
