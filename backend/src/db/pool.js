d2e39f2841beb83dfcd84bc2c0b87593479aec41
Gaspar Lazarte <lazartegaspar@gmail.com>
2025-11-20 23:47:07 -0300

    Correccion de errores en el pool de conexiones

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2/promise"
 import path from "path"
+import dotenv from "dotenv"
 import { fileURLToPath } from "url"
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js"
 
@@ -8,7 +9,7 @@ const __dirname = path.dirname(__filename)
 
 dotenv.config({ path: path.join(__dirname, ".env.db") })
 
-export const pool = mysql.createPool({
+const pool = mysql.createPool({
 	host: DB_HOST,
 	port: DB_PORT,
 	user: DB_USER,
